429a89c298f3ff64be628fa60d21b699a4461ff4
kuldeep1628 <kuldeepchauhan1628@gmail.com>
2024-12-16 18:59:54 +0530

    Backend

---
+++ DoctorAppointment/backend/config/mongodb.js
@@ -0,0 +1,9 @@
+import mongoose, { connect } from "mongoose";
+
+const connectDB = async () => {
+
+    mongoose.connection.on('connected',()=> console.log("DB connected"))
+
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+}
+export default connectDB
---
+++ DoctorAppointment/backend/middlewares/authAdmin.js
@@ -0,0 +1,26 @@
+import JWT from 'jsonwebtoken'
+
+//admin auth middleware
+
+const authAdmin = async (req, res, next) => {
+
+    try {
+
+        const { atoken } = req.headers
+        if (!atoken) {
+            return res.json({ success: false, message: "Not Authorized Login Again" })
+        }
+        const token_decode = JWT.verify(atoken, process.env.JWT_SECRET)
+
+        if (token_decode != process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+            return res.json({ success: false, message: "Not Authorized Login Again" })
+        }
+
+        next()
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+export default authAdmin
